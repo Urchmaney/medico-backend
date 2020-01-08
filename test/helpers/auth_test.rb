008eb2517fd5b2a15f7058501c162ff22a029caa
Urchmaney <kingsobino@gmail.com>
2020-01-08 11:02:45 +0000

    Add token encode and decode helper class

---
+++ test/helpers/auth_test.rb
@@ -0,0 +1,9 @@
+class AppointmentTest < ActiveSupport::TestCase
+
+  test 'Auth encode and decodes' do
+    payload = {username: 'lushhy'}
+    token = Auth.issue(payload)
+    assert_not_nil(token)
+    assert_equal(Auth.decode(token)['username'], payload[:username])
+  end
+end
